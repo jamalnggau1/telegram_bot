cc7f68755a5cf23b047db82259a2fb5ee609f385
gradinarnn <gradinarnn@gmail.com>
2021-08-31 17:37:46 +0300

    добавлена возможность перехода к 10 минутным встречам

--- handlers/users/ten_minutes_meet.py
+++ handlers/users/ten_minutes_meet.py
@@ -9,4 +9,4 @@ from loader import dp
 
 @dp.message_handler(Command("fast_meet"))
 async def bot_start(message: types.Message, state: FSMContext):
-    await message.answer("🧙‍♀️ В тот же миг Алиса юркнула за ним следом, не думая о том, как же она будет выбираться обратно.",reply_markup=one_button(text_btn=" перейти к чат-боту с 10 минутными почтами",callback_data=change_meeting_status_callback.new(status="10"), url='t.me/fast_10_minute_meet_bot'))
+    await message.answer("🧙‍♀️ В тот же миг Алиса юркнула за ним следом, не думая о том, как же она будет выбираться обратно.",reply_markup=one_button(text_btn=" перейти к чат-боту с 10 минутными почтами",callback_data=change_meeting_status_callback.new(status="10"), url='t.me/fast_10_minute_meet_bot?start'))
