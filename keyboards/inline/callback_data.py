c043235a3c66493136a0b8fbbfb1dc3a18d8cea3
gradinarnn <gradinarnn@gmail.com>
2021-09-14 20:59:01 +0300

    коммит для докера

--- keyboards/inline/callback_data.py
+++ keyboards/inline/callback_data.py
@@ -3,6 +3,6 @@ from aiogram.utils.callback_data import CallbackData
 change_meeting_status_callback = CallbackData("func", "status")
 edite_profile_callback = CallbackData("first_button", "status")
 meeting_status_callback = CallbackData("first_button", "status")
-checking_meeting= CallbackData("first_button", "status")
+checking_meeting = CallbackData("first_button", "status")
 meeting_feedback_wednesday= CallbackData("prefix", "status")
 help_callbackdata = CallbackData("prefix", "status")
