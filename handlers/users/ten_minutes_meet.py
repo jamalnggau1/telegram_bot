028dc696c5d4e9e6a984f3a2855f176cf0376560
gradinarnn <gradinarnn@gmail.com>
2021-09-23 11:25:11 +0300

    refactor fast_meet button

--- handlers/users/ten_minutes_meet.py
+++ handlers/users/ten_minutes_meet.py
@@ -10,6 +10,6 @@ from loader import dp
 @dp.message_handler(Command("fast_meet"))
 async def fast_meet_start(message: types.Message, state: FSMContext):
     await message.answer("🧙‍♀️ В тот же миг Алиса юркнула за ним следом, не думая о том, как же она будет выбираться обратно.",
-                         reply_markup=one_button(text_btn=" перейти к чат-боту с 10 минутными встречами",
+                         reply_markup=one_button(text_btn="🔮 перейти к 10-минутным встречам 🧙‍♀️",
                                                  callback_data=change_meeting_status_callback.new(status="10"),
                                                  url='t.me/EvgTG_bot?start=666'))
